e6cbedac76437af5ee4f08bcf351c0da8a95f009
Boris Kogan <fierval@gmail.com>
2021-08-14 15:19:16 -0700

    clustering

--- src/quiz/cluster/cluster.cpp
+++ src/quiz/cluster/cluster.cpp
@@ -75,15 +75,35 @@ void render2DTree(Node* node, pcl::visualization::PCLVisualizer::Ptr& viewer, Bo
 
 }
 
+void clusterHelper(int idx, const std::vector<std::vector<float>>& points, std::vector<int>& cluster, std::vector<bool>& processed, KdTree* tree, float distanceTol) {
+
+  processed[idx] = true;
+  cluster.push_back(idx);
+
+  std::vector<int> nearest = tree->search(points[idx], distanceTol);
+  for (int id : nearest) {
+    if (!processed[id]) {
+      clusterHelper(id, points, cluster, processed, tree, distanceTol);
+    }
+  }
+}
+
 std::vector<std::vector<int>> euclideanCluster(const std::vector<std::vector<float>>& points, KdTree* tree, float distanceTol)
 {
 
-  // TODO: Fill out this function to return list of indices for each cluster
-
   std::vector<std::vector<int>> clusters;
+  std::vector<bool> processed(points.size(), false);
 
+  for(int i = 0; i < points.size(); i++) {
+    if (processed[i]) {
+      continue;
+    }
+
+    std::vector<int> cluster;
+    clusterHelper(i, points, cluster, processed, tree, distanceTol);
+    clusters.push_back(cluster);
+  }
   return clusters;
-
 }
 
 int main()
